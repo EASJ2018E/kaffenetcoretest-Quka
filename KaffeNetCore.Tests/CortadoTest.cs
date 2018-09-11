9dc33a21abc03b5100e709fa63850bbefc195667
Arlind Ukshini <quka93@gmail.com>
2018-09-11 12:11:53 +0200

    add cortado test, and flatwhite

--- KaffeNetCore.Tests/CortadoTest.cs
+++ KaffeNetCore.Tests/CortadoTest.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using kaffe;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KaffeNetCore.Tests
 {
 	[TestClass]
-    class CortadoTest
+    public class CortadoTest
     {
-    }
+	    [TestMethod]
+	    public void TestCortadoMlMælk()
+	    {
+		    // Arrange
+		    Cortado c = new Cortado();
+
+			// Act
+			int mlmælk = c.MlMælk();
+
+			// Assert
+			Assert.AreEqual(25, mlmælk);
+	    }
+
+	    [TestMethod]
+	    public void TestCortadoPris()
+	    {
+		    // Arrange
+		    Cortado c = new Cortado();
+
+		    // Act
+		    int pris = c.Pris();
+
+		    // Assert
+		    Assert.AreEqual(25, pris);
+	    }
+	}
 }
--- KaffeNetCore.Tests/FlatWhiteTest.cs
+++ KaffeNetCore.Tests/FlatWhiteTest.cs
@@ -1,10 +1,48 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using KaffeNetCore.kaffe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KaffeNetCore.Tests
 {
 	[TestClass]
 	public class FlatWhiteTest
 	{
-		
+		[TestMethod]
+		public void TestFlatWhiteMlMælk()
+		{
+			// Arrange
+			FlatWhite fw = new FlatWhite();
+
+			// Act
+			int mlmælk = fw.MlMælk();
+
+			// Assert
+			Assert.AreEqual(160, mlmælk);
+		}
+
+		[TestMethod]
+		public void TestFlatWhitePris()
+		{
+			// Arrange
+			FlatWhite fw = new FlatWhite();
+
+			// Act
+			int pris = fw.Pris();
+
+			// Assert
+			Assert.AreEqual(45, pris);
+		}
+
+		[TestMethod]
+		public void TestFlatWhiteStyrke()
+		{
+			// Arrange
+			FlatWhite fw = new FlatWhite();
+
+			// Act
+			string styrke = fw.Styrke();
+
+			// Assert
+			Assert.AreEqual("Mild", styrke);
+		}
 	}
 }
